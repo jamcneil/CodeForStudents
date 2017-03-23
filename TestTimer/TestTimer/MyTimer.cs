13d695cfde9d38f27351d9c518e919b3dd09a98f
jamcneil <judy.mcneil@xamarin.com>
2017-03-23 10:43:48 -0700

    For Kashif
    A quick timer demo for Android

---
+++ TestTimer/TestTimer/MyTimer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Android.OS;
+
+namespace TestTimer
+{
+	public class MyTimer: CountDownTimer
+	{
+		Action onTick;
+		Action onFinish;
+
+		public MyTimer(long millisInFuture, long countDownInterval, Action onTick, Action onFinish)
+			:base(millisInFuture, countDownInterval)
+		{
+			this.onTick = onTick;
+			this.onFinish = onFinish;
+
+		}
+
+		public override void OnFinish ()
+		{
+			onFinish ();
+		}
+
+		public override void OnTick (long millisUntilFinished)
+		{
+			onTick ();
+		}
+	}
+}
---
+++ TestTimer/TestTimer/obj/Debug/android/src/md50a6d2e25c3099f84ff1bdebbad4f41de/MyTimer.java
@@ -0,0 +1,56 @@
+package md50a6d2e25c3099f84ff1bdebbad4f41de;
+
+
+public class MyTimer
+	extends android.os.CountDownTimer
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onFinish:()V:GetOnFinishHandler\n" +
+			"n_onTick:(J)V:GetOnTick_JHandler\n" +
+			"";
+		mono.android.Runtime.register ("TestTimer.MyTimer, TestTimer, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MyTimer.class, __md_methods);
+	}
+
+
+	public MyTimer (long p0, long p1) throws java.lang.Throwable
+	{
+		super (p0, p1);
+		if (getClass () == MyTimer.class)
+			mono.android.TypeManager.Activate ("TestTimer.MyTimer, TestTimer, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "System.Int64, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Int64, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public void onFinish ()
+	{
+		n_onFinish ();
+	}
+
+	private native void n_onFinish ();
+
+
+	public void onTick (long p0)
+	{
+		n_onTick (p0);
+	}
+
+	private native void n_onTick (long p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
